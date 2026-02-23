96ffcdd67551b3663cf7f8e50965524fea7cb92c
Jayant-129 <agrawaljayant86@gmail.com>
2026-02-23 14:26:44 +0530

    Coded UC3 and Updated Guessing App to Print Hint

--- GuessingApp.java
+++ GuessingApp.java
@@ -1,11 +1,12 @@
 
 /*
-    Guessing App - UC1- Game Initialization
+    Guessing App -  Game Initialization
     -This Class Serve as entry point for application
     -It Intialize the game configuration and display the rules
+    -Generate Hint for User on Wrong Guess
 
     @author - developer
-    @version - 2.0
+    @version - 3.0
 */
 
 import java.util.Scanner;
@@ -17,14 +18,18 @@ public class GuessingApp {
         gameConfig.showRules();
 
         Scanner scanner = new Scanner(System.in);
-        int attempts = 0;
+        int attempts = 0 , hintUsed = 0;
         while(attempts < gameConfig.getMaxAttempts()){
             System.out.println("Enter your guess: ");
             int guess = scanner.nextInt();
             attempts++;
 
             String result = GuessValidator.validateGuess(guess,gameConfig.getTargetNumber());
-            
+
+            if(!"CORRECT".equals(result) && hintUsed < gameConfig.getMaxHint()){
+                hintUsed++;
+                System.out.println(HintService.generateHint(gameConfig.getTargetNumber(),hintUsed));
+            }
             if("CORRECT".equals(result)){
                 break;
             }
